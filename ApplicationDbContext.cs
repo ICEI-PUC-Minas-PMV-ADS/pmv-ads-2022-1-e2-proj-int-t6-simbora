0462ff3b73e80a8434222257a2d791b1303e4f1a
luciaberger <91221827+luciaberger@users.noreply.github.com>
2022-06-12 18:35:52 -0300

    Models

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebBackEnd_Simbora.Models
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+        {
+        }
+
+        public DbSet<Evento> Eventos { get; set; }
+
+        public DbSet<Comentarios> Comentario { get; set; }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
